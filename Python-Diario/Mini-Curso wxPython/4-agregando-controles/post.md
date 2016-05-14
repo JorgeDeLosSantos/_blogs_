f402b4f4c9e6ff11e56dbb6579077b87a599da40
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-05-14 15:29:09 -0500

    Algunos cambios en títulos

---
+++ Python-Diario/Mini-Curso wxPython/4-agregando-controles/post.md
@@ -0,0 +1,7 @@
+# Agregando controles
+
+
+
+<button type="button" style="background-color: rgba(102,217,255,0.6); border-radius: 5px; box-shadow: 2px #ffaaff; padding: 5px;">
+<a href="http://www.pythondiario.com/2016/03/mini-curso-de-wxpython-1-introduccion.html#sumario-del-curso" style="color: #505050"><strong>Volver al índice</strong></a></button>
+
---
+++ Python-Diario/Mini-Curso wxPython/4-agregando-controles/test_app.py
@@ -0,0 +1,21 @@
+# -*- coding: utf8 -*-
+import wx
+
+class MiFrame(wx.Frame):
+    def __init__(self,*args,**kwargs):
+        wx.Frame.__init__(self,*args,**kwargs)
+        
+        self.text = wx.StaticText(self, wx.ID_ANY, "Etiqueta", pos=(10,100), size=(100,-1))
+        #~ self.text2 = wx.StaticText(self, wx.ID_ANY, "Etiqueta", pos=(100,100), size=(100,-1))
+        self.text.SetBackgroundColour("#ff00ff")
+        
+        self.Layout()
+        self.Fit()
+        self.Show()
+
+
+            
+if __name__=='__main__':
+    app = wx.App() 
+    fr = MiFrame(None, -1, "wxPython App", size=(300,200))
+    app.MainLoop()
